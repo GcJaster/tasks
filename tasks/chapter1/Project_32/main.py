fb01e4f1f78e1c67fcee1b1ca0ed5ef1b35384d0
gcjester <serg21322@yandex.ru>
2021-10-16 20:40:51 +0300

    interesting complete

---
+++ tasks/chapter1/Project_32/main.py
@@ -0,0 +1,7 @@
+# num = str(input("Введите 4значное число: "))
+# sum = 0
+# for elem in num:
+#     if elem.isdigit():
+#         sum += int(elem)
+#         print(elem, '+ ', end='')
+# print("=", sum)
